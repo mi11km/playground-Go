0a9a37841e0097b9f16ed88efaef823eb6157d65
mi11km <mi11km.basket@gmail.com>
2021-02-11 04:19:36 +0900

    question 1.3

---
+++ coding/chapter1/question3.go
@@ -0,0 +1,65 @@
+package chapter1
+
+import "strings"
+
+func URLifyWithStringsPackage(str string, length int) string {
+	var spaces int
+	for _, r := range str {
+		if r == ' ' {
+			spaces++
+		}
+	}
+	replaces := spaces - len(str) + length
+	return strings.Replace(strings.Replace(str, " ", "%20", replaces), " ", "", -1)
+}
+
+func URLify(str string, length int) string {
+	var spaces int
+	for _, r := range str {
+		if r == ' ' {
+			spaces++
+		}
+	}
+	replaces := spaces - len(str) + length
+	runes := make([]rune, length+replaces*2)
+	i := 0
+	for _, r := range str {
+		if r == ' ' {
+			if replaces > 0 {
+				runes[i] = '%'
+				runes[i+1] = '2'
+				runes[i+2] = '0'
+				i += 3
+				replaces--
+			}
+		} else {
+			runes[i] = r
+			i++
+		}
+	}
+	return string(runes)
+}
+
+func URLifyArray(runes []rune, length int) string {
+	var spaces, index, i int
+	for i = 0; i < length; i++ {
+		if runes[i] == ' ' {
+			spaces++
+		}
+	}
+	index = length + spaces*2 - 1
+	str := make([]rune, index+1)
+	index = 0
+	for i = 0; i < length; i++ {
+		if runes[i] == ' ' {
+			str[index] = '%'
+			str[index+1] = '2'
+			str[index + 2] = '0'
+			index += 3
+		} else {
+			str[index] = runes[i]
+			index++
+		}
+	}
+	return string(str)
+}
---
+++ coding/chapter1/question3_test.go
@@ -0,0 +1,82 @@
+package chapter1
+
+import "testing"
+
+func TestURLifyArray(t *testing.T) {
+	type args struct {
+		runes  []rune
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "success", args: args{runes: []rune("Mr John Smith "), length: 13}, want: "Mr%20John%20Smith"},
+		{name: "success", args: args{runes: []rune(" Mr John Smith "), length: 14}, want: "%20Mr%20John%20Smith"},
+		{name: "success", args: args{runes: []rune(" Mr John Smith "), length: 15}, want: "%20Mr%20John%20Smith%20"},
+		{name: "success", args: args{runes: []rune("Hello"), length: 5}, want: "Hello"},
+		{name: "success", args: args{runes: []rune("  "), length: 2}, want: "%20%20"},
+		{name: "success", args: args{runes: []rune("  "), length: 1}, want: "%20"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := URLifyArray(tt.args.runes, tt.args.length); got != tt.want {
+				t.Errorf("URLifyWithStringsPackage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestURLifyWithStringsPackage(t *testing.T) {
+	type args struct {
+		str    string
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "success", args: args{str: "Mr John Smith ", length: 13}, want: "Mr%20John%20Smith"},
+		{name: "success", args: args{str: " Mr John Smith ", length: 14}, want: "%20Mr%20John%20Smith"},
+		{name: "success", args: args{str: " Mr John Smith ", length: 15}, want: "%20Mr%20John%20Smith%20"},
+		{name: "success", args: args{str: "Hello", length: 5}, want: "Hello"},
+		{name: "success", args: args{str: "  ", length: 2}, want: "%20%20"},
+		{name: "success", args: args{str: "  ", length: 1}, want: "%20"},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := URLifyWithStringsPackage(tt.args.str, tt.args.length); got != tt.want {
+				t.Errorf("URLifyWithStringsPackage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestURLify(t *testing.T) {
+	type args struct {
+		str    string
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "success", args: args{str: "Mr John Smith ", length: 13}, want: "Mr%20John%20Smith"},
+		{name: "success", args: args{str: " Mr John Smith ", length: 14}, want: "%20Mr%20John%20Smith"},
+		{name: "success", args: args{str: " Mr John Smith ", length: 15}, want: "%20Mr%20John%20Smith%20"},
+		{name: "success", args: args{str: "Hello", length: 5}, want: "Hello"},
+		{name: "success", args: args{str: "  ", length: 2}, want: "%20%20"},
+		{name: "success", args: args{str: "  ", length: 1}, want: "%20"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := URLify(tt.args.str, tt.args.length); got != tt.want {
+				t.Errorf("Urlify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
