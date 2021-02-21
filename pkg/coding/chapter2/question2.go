7484f7d6a2429cf6fd816c3d10070c9f005b76d4
mi11km <mi11km.basket@gmail.com>
2021-02-21 16:30:54 +0900

    question 2.2

---
+++ pkg/coding/chapter2/question2.go
@@ -0,0 +1,33 @@
+package chapter2
+
+func (l *SinglyLinkedList) GetFromRear(index int) int {
+	if index < 0 || l.length < index {
+		return -1
+	}
+	n := l.head
+	for i := 0; i < l.length-index; i++ {
+		n = n.next
+	}
+	return n.value
+}
+
+// 長さがわかってない場合
+func (l *SinglyLinkedList) KthToLast(index int) int {
+	if index < 0 {
+		return -1
+	}
+	p1 := l.head
+	p2 := l.head
+	for i := 0; i < index; i++ {
+		if p1 == nil {
+			return -1
+		}
+		p1 = p1.next
+	}
+
+	for p1 != nil {
+		p1 = p1.next
+		p2 = p2.next
+	}
+	return p2.value
+}
---
+++ pkg/coding/chapter2/question2_test.go
@@ -0,0 +1,67 @@
+package chapter2
+
+import "testing"
+
+func TestSinglyLinkedList_GetFromRear(t *testing.T) {
+
+	type args struct {
+		index int
+		list  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "success", args: args{index: 1, list: []int{1, 2, 3, 4}}, want: 4},
+		{name: "success", args: args{index: 2, list: []int{1, 2, 3, 4}}, want: 3},
+		{name: "success", args: args{index: 3, list: []int{1, 2, 3, 4}}, want: 2},
+		{name: "success", args: args{index: 4, list: []int{1, 2, 3, 4}}, want: 1},
+		{name: "index out of range", args: args{index: 5, list: []int{1, 2, 3, 4}}, want: -1},
+		{name: "index out of range", args: args{index: -1, list: []int{1, 2, 3, 4}}, want: -1},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := NewSinglyLinkedList()
+			for _, val := range tt.args.list {
+				l.Insert(val)
+			}
+			if got := l.GetFromRear(tt.args.index); got != tt.want {
+				t.Errorf("GetFromRear() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSinglyLinkedList_KthToLast(t *testing.T) {
+
+	type args struct {
+		index int
+		list  []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "success", args: args{index: 1, list: []int{1, 2, 3, 4}}, want: 4},
+		{name: "success", args: args{index: 2, list: []int{1, 2, 3, 4}}, want: 3},
+		{name: "success", args: args{index: 3, list: []int{1, 2, 3, 4}}, want: 2},
+		{name: "success", args: args{index: 4, list: []int{1, 2, 3, 4}}, want: 1},
+		{name: "index out of range", args: args{index: 5, list: []int{1, 2, 3, 4}}, want: -1},
+		{name: "index out of range", args: args{index: -1, list: []int{1, 2, 3, 4}}, want: -1},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := NewSinglyLinkedList()
+			for _, val := range tt.args.list {
+				l.Insert(val)
+			}
+			if got := l.KthToLast(tt.args.index); got != tt.want {
+				t.Errorf("KthToLast() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
