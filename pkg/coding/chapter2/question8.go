13848f9f050f912af6974ab0c87b17d40581c7dc
mi11km <mi11km.basket@gmail.com>
2021-03-08 22:49:43 +0900

    question2.8

---
+++ pkg/coding/chapter2/question8.go
@@ -0,0 +1,26 @@
+package chapter2
+
+/* 循環する連結リストの循環点を返す関数 */
+func (l *SinglyLinkedList) FindLoopNode() *node {
+	slow := l.head
+	fast := l.head
+
+	for fast != nil && fast.next != nil {
+		slow = slow.next
+		fast = fast.next.next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.next == nil {
+		return nil
+	}
+
+	slow = l.head
+	for slow != fast {
+		slow = slow.next
+		fast = fast.next
+	}
+	return fast
+}
