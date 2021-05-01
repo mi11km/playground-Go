73088d95eeeb2c686cd65b04928c4474ee883d1b
mi11km <mi11km.basket@gmail.com>
2021-05-02 02:12:28 +0900

    implement chat app

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"app/chat"
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("chat/templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	addr := flag.String("addr", ":8080", "appのアドレス")
+	flag.Parse()
+
+	r := chat.NewRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.Run()
+
+	log.Println("webサーバーを開始。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
